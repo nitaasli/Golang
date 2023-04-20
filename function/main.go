35c94fa04f3fd6bee23cef2ff9939a35d5484f49
nitas <nitas.septiani@gmail.com>
2023-04-20 19:13:02 +0700

    addsub

--- function/main.go
+++ function/main.go
@@ -2,11 +2,22 @@ package main
 
 import "fmt"
 
-func nita(name string) {
-	fmt.Printf("Hallo %s\n", name)
+func nita(name string) (text string) {
+	text = "Hallo " + name
+	return
+}
 
+func addSub(x, y int) (int, int) {
+	return x + y, x - y
+}
+
+func add(x, y int) int {
+	return x + y
 }
 
 func main() {
-	nita(("manusia"))
+	// nita(("manusia"))
+	// fmt.Println(nita("manusia"))
+	// fmt.Println(add(2, 5))
+	fmt.Println(addSub(2, 5))
 }
