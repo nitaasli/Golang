b3465b9fdd6bea255312779ad7e471eb28388069
nitas <nitas.septiani@gmail.com>
2023-04-25 15:35:03 +0700

    pointer

--- pointer/main.go
+++ pointer/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
+func incrementPtr(var1 *int) {
+	*var1 = *var1 + 1
+}
+
+func increment(var1 int) {
+	var1 = var1 + 1
+
+}
 func main() {
+	var1 := 1
+	fmt.Printf("my value is %d\n", var1)
+	increment(var1)
+	fmt.Printf("my value is %d\n", var1)
+	incrementPtr(&var1)
+	fmt.Printf("my address is %X, value is %d\n", &var1, var1)
 
 }
